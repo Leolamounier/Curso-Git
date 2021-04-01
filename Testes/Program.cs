01c04eb778338815556e7b75364642ce22b73b9d
Leonardo Lamounier <leomail2@hotmail.com>
2021-04-01 01:35:25 -0300

    Projeto iniciado mostrando a mensagem Ola Mundo

---
+++ Testes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Testes {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Olá mundo!");
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
+
+
